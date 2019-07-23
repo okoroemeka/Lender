c4aab7db402ea7edbc262bd15f40443736afb60d
okoroemeka <solomonokoro056@gmail.com>
2019-07-23 12:35:10 +0100

    .gitignore fix

--- dist/utils/tokenHelpe.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jwt = require("jsonwebtoken");
-// import {  } from "js";
-exports.createToken = (data, expiresIn, secreteKey) => {
-    return jwt.sign(data, secreteKey, { expiresIn });
-};
-//# sourceMappingURL=tokenHelpe.js.map
